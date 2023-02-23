0111c88b51b285f3388d8691a8f6a44edb89d5e3
Neelam <neelamn@mindfiresolutions.com>
2023-02-23 23:22:07 +0530

    second-commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello Hufflepuff</div>;
+  return (
+    <div className="App">
+      Hello Hufflepuff
+      <p>Second update</p>
+    </div>
+  );
 }
 
 export default App;
